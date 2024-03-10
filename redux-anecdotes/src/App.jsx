02b4377492d56feed3aed4744f3890b411626c50
juaxE <juho.kirvesmaki@helsinki.fi>
2024-03-10 15:39:54 +0200

    6.9

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,13 +1,15 @@
 import AnecdoteList from './components/AnecdoteList.jsx'
 import AnecdoteForm from './components/AnecdoteForm.jsx'
+import Filter from './components/Filter.jsx'
 
 const App = () => {
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <AnecdoteForm />
+      <Filter />
       <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
