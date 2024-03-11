8fb0010f5a68151cb506fa6318dec9c390b9834f
juaxE <juho.kirvesmaki@helsinki.fi>
2024-03-11 15:48:17 +0200

    6.22

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hours spent this far: 5
+Hours spent this far: 7
---
+++ query-anecdotes/src/App.jsx
@@ -0,0 +1,63 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
+
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+
+  const handleVote = (anecdote) => {
+    const newAnecdote = { ...anecdote, votes: anecdote.votes + 1 }
+    updatedAnecdoteMutation.mutate(newAnecdote)
+  }
+
+  const updatedAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (data) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      const newAnecdotes = anecdotes.map((anecdote) => anecdote.id !== data.data.id ? anecdote : data.data)
+      queryClient.setQueryData(['anecdotes'], newAnecdotes)
+    }
+  },)
+
+  const { isPending, isError, data } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => getAnecdotes
+  },)
+
+  console.log(JSON.stringify(data))
+
+  if (isPending) {
+    return <div>loading data...</div>
+  }
+  if (isError) {
+    return <span>anecdote service not available due to problems in server</span>
+  }
+
+  const anecdotes = data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
